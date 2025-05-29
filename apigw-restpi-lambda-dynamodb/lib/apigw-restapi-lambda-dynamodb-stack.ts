43f390e394e15ded375b8cff61cdd9e77ece8a3a
Luan Pedro <xdlpedroxxd@icloud.com>
2025-05-29 14:53:09 -0300

    refactor: comment reservedConcurrentExecutions because i use free tier

--- apigw-restpi-lambda-dynamodb/lib/apigw-restapi-lambda-dynamodb-stack.ts
+++ apigw-restpi-lambda-dynamodb/lib/apigw-restapi-lambda-dynamodb-stack.ts
@@ -10,23 +10,27 @@ import {
   Table,
   TableV2,
 } from "aws-cdk-lib/aws-dynamodb";
+import { LogGroup } from "aws-cdk-lib/aws-logs";
 
 export class ApiGwRestApiLambdaDynamodbStack extends Cdk.Stack {
   constructor(scope: Construct, id: string, props?: Cdk.StackProps) {
     super(scope, id, props);
 
-    const dynamoDbTable = new TableV2(this, "Users", {
+    const dynamoDbGlobalTable = new TableV2(this, "Users", {
       removalPolicy: Cdk.RemovalPolicy.DESTROY,
       partitionKey: { name: "id", type: AttributeType.STRING },
       replicas: [{ region: "us-west-1" }],
     });
 
+    // esbuild compact them js modules and help optimize cold start
     const lambdaBackend = new NodejsFunction(this, "LambdaCRUD", {
       runtime: AwsLambda.Runtime.NODEJS_LATEST,
+      architecture: AwsLambda.Architecture.ARM_64, //Optimize cold start
+     // reservedConcurrentExecutions: 100, //evitar overhead nos serviços downstreams, porem dropa novas conexoes se o pool estiver no limite 
       handler: "handler",
       timeout: Cdk.Duration.seconds(300),
       entry: Path.join(__dirname, "../lambda/app.ts"),
-      environment: { TABLE_NAME: dynamoDbTable.tableName },
+      environment: { TABLE_NAME: dynamoDbGlobalTable.tableName },
       bundling: {
         commandHooks: {
           afterBundling: (inputDir: string, outputDir: string): string[] => [],
@@ -36,21 +40,26 @@ export class ApiGwRestApiLambdaDynamodbStack extends Cdk.Stack {
       },
     });
 
-    dynamoDbTable.grantReadWriteData(lambdaBackend);
+   const prodAlias = new AwsLambda.Alias(this, 'ProdAlias', {
+      aliasName: 'prod',
+      version: lambdaBackend.currentVersion,
+      provisionedConcurrentExecutions: 1, // optimize cold start
+    });
+
+    dynamoDbGlobalTable.grantReadWriteData(lambdaBackend);
+    //Define LogGroup Name in CloudWatch
+    const logGroup = new LogGroup(this, "ApiGwRestApiLambdaDynamodbStack");
 
     const apiGw = new ApiGw.RestApi(this, "RestAPI", {
       deployOptions: {
         dataTraceEnabled: true,
         tracingEnabled: true,
+        accessLogDestination: new ApiGw.LogGroupLogDestination(logGroup)
       },
     });
 
     const apiGwResource = apiGw.root.addResource("users");
     apiGwResource.addMethod("GET", new ApiGw.LambdaIntegration(lambdaBackend));
     apiGwResource.addMethod("POST", new ApiGw.LambdaIntegration(lambdaBackend));
-    apiGwResource.addMethod(
-      "DELETE",
-      new ApiGw.LambdaIntegration(lambdaBackend),
-    );
   }
 }
